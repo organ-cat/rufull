1cb0710b7a7c4fc5cd254dc20e131a1ca26bfa6c
zjwhome <465007620@qq.com>
2017-11-29 23:00:22 +0800

    不要提交

--- src/main/java/com/cat/rufull/app/user/UserController.java
+++ src/main/java/com/cat/rufull/app/user/UserController.java
@@ -1,5 +1,6 @@
 package com.cat.rufull.app.user;
 
+import com.cat.rufull.domain.common.util.PaymentUtil;
 import com.cat.rufull.domain.model.User;
 import com.cat.rufull.domain.service.user.IUserService;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/user")
@@ -22,5 +24,92 @@ public class UserController {
         model.addAttribute("user", user);
         System.out.println(user.getUserName());
         return "showUser";
+    }
+
+    @RequestMapping("/order")
+    public String showpay(){
+        return "order";
+    }
+
+    @RequestMapping("/orderPay")
+    public void toPay(String pd_FrpId, HttpServletResponse response) throws Exception{
+        // 付款:
+        // 定义付款的参数:
+        String p0_Cmd = "Buy";
+        String p1_MerId = "10001126856";
+        String p2_Order = "";
+        String p3_Amt = "0.01";
+        String p4_Cur = "CNY";
+        String p5_Pid = "";
+        String p6_Pcat = "";
+        String p7_Pdesc = "";
+        String p8_Url = "http://192.168.253.1:80/rufull/user/callBack.action";
+        String p9_SAF = "";
+        String pa_MP = "";
+        String pr_NeedResponse = "1";
+        String keyValue = "69cl522AV6q613Ii4W6u8K6XuW8vM1N6bFgyv769220IuYe9u37N4y7rI4Pl";
+        String hmac = PaymentUtil.buildHmac(p0_Cmd, p1_MerId, p2_Order, p3_Amt, p4_Cur, p5_Pid, p6_Pcat, p7_Pdesc, p8_Url, p9_SAF, pa_MP,pd_FrpId , pr_NeedResponse, keyValue);
+
+        StringBuffer sb = new StringBuffer("https://www.yeepay.com/app-merchant-proxy/node?");
+        sb.append("p0_Cmd=").append(p0_Cmd).append("&");
+        sb.append("p1_MerId=").append(p1_MerId).append("&");
+        sb.append("p2_Order=").append(p2_Order).append("&");
+        sb.append("p3_Amt=").append(p3_Amt).append("&");
+        sb.append("p4_Cur=").append(p4_Cur).append("&");
+        sb.append("p5_Pid=").append(p5_Pid).append("&");
+        sb.append("p6_Pcat=").append(p6_Pcat).append("&");
+        sb.append("p7_Pdesc=").append(p7_Pdesc).append("&");
+        sb.append("p8_Url=").append(p8_Url).append("&");
+        sb.append("p9_SAF=").append(p9_SAF).append("&");
+        sb.append("pa_MP=").append(pa_MP).append("&");
+        sb.append("pd_FrpId=").append(pd_FrpId).append("&");
+        sb.append("pr_NeedResponse=").append(pr_NeedResponse).append("&");
+        sb.append("hmac=").append(hmac);
+
+        response.sendRedirect(sb.toString());
+
+    }
+
+    @RequestMapping("/backPay")
+    public void backPay(){
+        //退款:
+        // 定义退款的参数:
+        String p0_Cmd = "RefundOrd";
+        String p1_MerId = "10001126856";
+        String pb_TrxId = "";
+        String p3_Amt = "0.01";
+        String p4_Cur = "CNY";
+        String p5_Desc = "";
+        String keyValue = "69cl522AV6q613Ii4W6u8K6XuW8vM1N6bFgyv769220IuYe9u37N4y7rI4Pl";
+        String hmac = PaymentUtil.buildHmac(p0_Cmd, p1_MerId, pb_TrxId, p3_Amt, p4_Cur, p5_Desc, keyValue);
+
+        StringBuffer sb = new StringBuffer("https://www.yeepay.com/app-merchant-proxy/node?");
+        sb.append("p0_Cmd=").append(p0_Cmd).append("&");
+        sb.append("p1_MerId=").append(p1_MerId).append("&");
+        sb.append("p2_Order=").append(p2_Order).append("&");
+        sb.append("p3_Amt=").append(p3_Amt).append("&");
+        sb.append("p4_Cur=").append(p4_Cur).append("&");
+        sb.append("p5_Pid=").append(p5_Pid).append("&");
+        sb.append("p6_Pcat=").append(p6_Pcat).append("&");
+        sb.append("p7_Pdesc=").append(p7_Pdesc).append("&");
+        sb.append("p8_Url=").append(p8_Url).append("&");
+        sb.append("p9_SAF=").append(p9_SAF).append("&");
+        sb.append("pa_MP=").append(pa_MP).append("&");
+        sb.append("pd_FrpId=").append(pd_FrpId).append("&");
+        sb.append("pr_NeedResponse=").append(pr_NeedResponse).append("&");
+        sb.append("hmac=").append(hmac);
+
+        response.sendRedirect(sb.toString());
+    }
+
+    /**
+     * 付款成功后的回调方法
+     */
+    @RequestMapping("/callBack")
+    public String callBack(Model model){
+        String msg = "订单付款成功!订单号:1111111,付款金额:0.01";
+        model.addAttribute("msg", msg);
+
+        return "msg";
     }
 }
