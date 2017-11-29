c4c53b19014dad048dc527535718d76e9d01629c
Luckily <1451179739@qq.com>
2017-11-29 09:40:48 +0800

    manager实体类生成

---
+++ src/main/java/com/cat/rufull/domain/model/ManagerLog.java
@@ -0,0 +1,64 @@
+package com.cat.rufull.domain.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Luckily on 2017/11/29.
+ */
+public class ManagerLog implements Serializable{
+    private int id;
+    private String detail;
+    private int type;
+    private Date createtime;
+    private int managerid;
+    private int accountid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public int getManagerid() {
+        return managerid;
+    }
+
+    public void setManagerid(int managerid) {
+        this.managerid = managerid;
+    }
+
+    public int getAccountid() {
+        return accountid;
+    }
+
+    public void setAccountid(int accountid) {
+        this.accountid = accountid;
+    }
+}
