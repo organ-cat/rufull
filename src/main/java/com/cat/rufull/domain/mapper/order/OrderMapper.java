dcc26cabbe89920ca93d8f06975cf282bd976108
ColdZhong <ColdZhong@gmail.com>
2017-11-30 08:39:07 +0800

    最新的DDL和DML，请组员各自拉取。

--- src/main/java/com/cat/rufull/domain/mapper/order/OrderMapper.java
+++ src/main/java/com/cat/rufull/domain/mapper/order/OrderMapper.java
@@ -7,8 +7,16 @@ import com.cat.rufull.domain.model.Order;
 import java.util.List;
 
 public interface OrderMapper {
+    // 内容:
+    //  下单时间
+    //  下单内容:商店名,商店图片,每个订单项的商品名和数量,总商品数量(统计得出),订单号
+    //  支付金额:总额,支付方式
+    //  订单状态
     public List<Order> findOrderByAccountId(Integer accountId);
 
+    // 用订单号查订单详情
+    // 内容:
+    //  商店名,商店图片,订单号,商家电话,订单内容
     public Order findOrderById(Integer id);
 
     public void updateOrder(Order order);
